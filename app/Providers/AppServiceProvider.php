91720971f9092308ea2e3591351c5848d1162ec4
Alba <alba.alonso@ikasle.egibide.org>
2023-03-10 16:00:37 +0100

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
        PaginationPaginator::useBootstrapFive();
+
+       if (env('REDIRECT_HTTPS')) {
+        $url->formatScheme('https://');
+    }
     }
 }
